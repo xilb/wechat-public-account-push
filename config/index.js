944becaac4644456b986a7093321390a2fc29319
xilb <113752345+xilb@users.noreply.github.com>
2022-09-17 17:37:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,21 +60,21 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "测试1",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oxOZB5wFYUM55O-Vx5F2BSgIAV_I",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "TuU0zqfb89bBX8_Lw7TQRZb0Xn3FgrWIwee24Z7QoY0",
       // 所在省份
-      province: "广东",
+      province: "山东",
       // 所在城市
-      city: "肇庆",
+      city: "青岛",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-25',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://astro.sina.com.cn/",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -97,7 +97,7 @@ export const config = {
     {
       name: "老婆1",
       id: "",
-      useTemplateId: "",
+      useTemplateId: "TuU0zqfb89bBX8_Lw7TQRZb0Xn3FgrWIwee24Z7QoY0",
       province: "",
       city: "",
       horoscopeDate: '',
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "31uxNNQgb1TLanOtX9MZJk-C78_b8Vgfc6cfEQkVTBU",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -147,7 +147,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oxOZB5wFYUM55O-Vx5F2BSgIAV_I",
     },
     // 你可以不断按格式往下增加
     // ...
